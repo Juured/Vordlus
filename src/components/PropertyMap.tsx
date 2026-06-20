37fa64c57559c5f50da753c20d92f893742f4ed5
Anja31 <Anna.Egorova@real.edu.ee>
2026-06-20 14:05:10 +0300

    Update PropertyMap.tsx

--- src/components/PropertyMap.tsx
+++ src/components/PropertyMap.tsx
@@ -1,9 +1,25 @@
 'use client';
 
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 export default function PropertyMap() {
   return (
-    <div className="h-[500px] w-full rounded-lg border border-rule bg-white p-4">
-      Kaart tuleb siia
+    <div className="h-[500px] w-full rounded-lg border bg-white p-0 overflow-hidden">
+      <MapContainer
+        center={[59.437, 24.7536]}
+        zoom={14}
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[59.437, 24.7536]}>
+          <Popup>Valitud asukoht</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
